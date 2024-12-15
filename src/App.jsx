1bb5396e9a738914118e8c707394b2cc4f33ff4b
O. HADALIN <dyblikat777@gmail.com>
2024-12-15 17:53:45 +0200

    Description to component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import Options from './components/Options/Options';
 import Feedback from './components/Feedback/Feedback';
 import Notification from './components/Notification/Notification';
+import Description from './components/Description/Description';
 import './App.css';
 
 const App = () => {
@@ -33,11 +34,7 @@ const App = () => {
 
   return (
     <div className="app-container">
-      <h1>Sip Happens Café</h1>
-      <p>
-        Please leave your feedback about our service by selecting one of the
-        options below.
-      </p>
+      <Description />
       <Options
         updateFeedback={updateFeedback}
         resetFeedback={resetFeedback}
